c50ce53a574df972acaff2e0c7794155a20bba0f
AbdulQadhir <kader.chalil@gmail.com>
2019-03-20 14:06:35 +0530

    community

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-//import App from './App';
-import App from "./src/App";
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App); 
+AppRegistry.registerComponent(appName, () => App);
